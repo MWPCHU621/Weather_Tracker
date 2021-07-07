80d0df6ffec016a18f8cec1ea116f69ce4ac4826
Patrick Chu <mingweipatrickc@gmail.com>
2021-07-07 16:18:10 -0400

    finished making layout of search bar without any onclick functions

--- src/Components/searchCity.js
+++ src/Components/searchCity.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 
-export class SearchCity extends React.Component {
+export function SearchCity() {
+    // const cityList = useSelector(fetchCityList);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <div class="searchBar_container">
+                <form>
+                    <input type="text" placeholder="Type City Name" name="searchBar"></input>
+                    <button type="submit" onClick="findCity()"><i class="fas fa-plus"></i></button>
+                </form>
+            </div>
+        </div>
+    );
+
+
 
 }
