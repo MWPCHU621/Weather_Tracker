9501a7ebd21479a30c8a83bc1bc04e72432cc9d5
Patrick Chu <mingweipatrickc@gmail.com>
2021-07-08 22:26:48 -0400

    updated addcity reducer to take into account duplicate cities being added

--- src/Reducers/SearchBarSlice.js
+++ src/Reducers/SearchBarSlice.js
@@ -11,16 +11,26 @@ export const searchBarSlice = createSlice({
 
    reducers: {
        addCity: (state, action) => {
+           //checks to see if the length of the list is already full at 8 items or not.
             if(state.cityList.length >= 8) {
                 state.cityList.pop();
             }
 
-            state.cityList.splice(0,0,action.payload);
+            //takes into account duplicate cities
+            let cityNameList = state.cityList.map(city => city.name);
+
+            
+            if(cityNameList.includes(action.payload.name)) { //checks to see if the city thats to be added already exists or not.
+                let index = cityNameList.indexOf(action.payload.name);
+                state.cityList.splice(index, 1, action.payload);
+            } else { //the city to be added doesn't already exist in the list.
+                state.cityList.splice(0,0,action.payload);
+            }
+
         },
 
         removeCity: (state, action) => {
-            console.log("REMOVE CITY DEBUG", state, action.payload.name);
-            state.cityList.filter(city => city.name != action.payload.name);
+            state = action.payload;
         }
    },
     
