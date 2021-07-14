ab223c4fc514275c871de60835a1e13759285a45
Patrick Chu <mingweipatrickc@gmail.com>
2021-07-14 00:22:07 -0400

    changed search bar input form to a self-made form for better simimlarity to design

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,27 +2,36 @@ import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import {addCity} from '../Reducers/CityListSlice';
 import { getCityInfo } from '../Helper/ApiCalls';
-import SearchBar from 'material-ui-search-bar';
+import AddIcon from '@material-ui/icons/Add';
 import '../style/searchBar.css';
 
 export function CitySearchBar() {
 
     const dispatch = useDispatch();
-    const [value, setValue] = useState("");
+    // const [value, setValue] = useState("");
+    const [cityName, setCityName] = useState("");
     const [error, setError] = useState(false);
     
     return (
         <div className="searchBar_container">
-            <SearchBar
-                value={value}
-                onChange={(newValue) => setValue(newValue)}
-                onRequestSearch={() => { searchCity(value); setValue(""); }} 
-                placeholder="Type City Name"
-                style={{boxShadow:"none"}}
-                />
+            <form onSubmit={handleSubmit} className="add_city_form">
+                <input type="text" className="add_city_textarea" size="28" value={cityName} onChange={handleChange} placeholder="Type City Name"/>
+                <button type="submit" className="add_city_btn"><AddIcon /></button>
+            </form>
+
             <p className={error ? 'show_error' : 'hide_error'}>Invalid city. Please enter a valid city</p>
         </div>
     );
+
+    function handleChange(e) {
+        setCityName(e.target.value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        searchCity(cityName);
+        setCityName("");
+    }
     
     function searchCity (city) {
          getCityInfo(city).then((data) => {
@@ -38,5 +47,4 @@ export function CitySearchBar() {
              setError(true);
          })
      }
-    
 }
